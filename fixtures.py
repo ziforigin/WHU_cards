a141b4a4282b5b1a8d3b05b803c6c883079f6c24
ziforigin <ziforigin@gmail.com>
2020-11-11 23:15:09 +0300

    Update fixtures.py

--- fixtures.py
+++ fixtures.py
@@ -5,6 +5,7 @@ import pytest
 @pytest.fixture
 def browser():
     print("\nstart browser for test..")
+    System.setProperty("webdriver.chrome.driver", "~/Downloads/WHU_cards/chromedriver")
     browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
