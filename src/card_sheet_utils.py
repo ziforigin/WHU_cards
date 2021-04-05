f007be5b27be4b2e41c4b0a37d65c9043052a261
iakovsidorov <ziforigin@gmail.com>
2021-04-05 22:00:18 +0300

    fixed issue with power cards back

--- src/card_sheet_utils.py
+++ src/card_sheet_utils.py
@@ -84,16 +84,6 @@ def divide_cards_to_sheets_and_save(name: str, images_list: list, config: DeckCo
 def generate_and_save_objective_cards_back(config: DeckConfig, file_paths: FilePaths):
     color = (158, 129, 97)
     file_name = f"{config.name}_objective_backs.png"
-    generate_and_save_cards_back(config, file_paths, color, file_name)
-
-
-def generate_and_save_power_cards_back(config: DeckConfig, file_paths: FilePaths):
-    color = (65, 73, 90)
-    file_name = f"{config.name}_power_backs.png"
-    generate_and_save_cards_back(config, file_paths, color, file_name)
-
-
-def generate_and_save_cards_back(config: DeckConfig, file_paths: FilePaths, color: tuple, file_name: str):
     images = []
     counter = 0
     back_file_path = file_paths.objective_card_back
@@ -102,5 +92,29 @@ def generate_and_save_cards_back(config: DeckConfig, file_paths: FilePaths, colo
         counter += 1
     logging.info(f'Generating card backs {file_name}')
     divide_cards_to_sheets_and_save(file_name, images, config, file_paths, color)
+
+
+def generate_and_save_power_cards_back(config: DeckConfig, file_paths: FilePaths):
+    color = (65, 73, 90)
+    file_name = f"{config.name}_power_backs.png"
+    images = []
+    counter = 0
+    back_file_path = file_paths.power_card_back
+    while counter < (config.cards_in_column * config.cards_in_row):
+        images.append(back_file_path)
+        counter += 1
+    logging.info(f'Generating card backs {file_name}')
+    divide_cards_to_sheets_and_save(file_name, images, config, file_paths, color)
+
+
+# def generate_and_save_cards_back(config: DeckConfig, file_paths: FilePaths, color: tuple, file_name: str):
+#     images = []
+#     counter = 0
+#     back_file_path = file_paths.objective_card_back
+#     while counter < (config.cards_in_column * config.cards_in_row):
+#         images.append(back_file_path)
+#         counter += 1
+#     logging.info(f'Generating card backs {file_name}')
+#     divide_cards_to_sheets_and_save(file_name, images, config, file_paths, color)
 
 
